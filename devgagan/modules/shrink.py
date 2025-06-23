29a2741914e685c53d680beb0f96663a51fe5304
jashanbeniwal0101 <beniwaljass733@gmail.com>
2025-06-23 12:47:14 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,7 +73,7 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://files.catbox.moe/w10e8w.jpg"
+        image_url = "https://i.ibb.co/Kj8w6YSq/image.jpg"
         join_button = InlineKeyboardButton("Join Channel", url="https://t.me/allbotsupdates1")
         premium = InlineKeyboardButton("Get Premium", url="https://t.me/arsh_beniwal")   
         keyboard = InlineKeyboardMarkup([
